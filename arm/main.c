ac5d85147b31a172c3af97f0830e927ebcc1214d
Michael Davidsaver <mdavidsaver@gmail.com>
2015-08-03 18:06:12 -0400

    arm: switch to vexpress-a9 board

--- arm/main.c
+++ arm/main.c
@@ -1,5 +1,5 @@
 /* Minimal bare metal arm program for use without a bootloader.
- * Parts are specific to ARM versatileab emulation w/ QEMU
+ * Parts are specific to ARM vexpress-a9 emulation w/ QEMU
  *
  * Michael Davidsaver <mdavidsaver@gmail.com>
  */
@@ -8,67 +8,18 @@ typedef __UINT16_TYPE__ uint16_t;
 typedef __UINT32_TYPE__ uint32_t;
 typedef __SIZE_TYPE__ size_t;
 
-extern char __bss_start, __bss_end;
-extern char __data_start, __data_end, __data_load;
-
 /* some variables to see if we're loading up the data and bss sections correctly */
-volatile char *ioportbase;
-int foobar;
+volatile uint32_t ioportbase;
+uint32_t foobar;
 volatile uint32_t ioportbase2 = 0xdeadbeef;
-uint32_t foobar2 = 0xbad1face;
+uint32_t foobar2 = 0x1badface;
 
-static inline
-void memmove(void *dst, const void *src, size_t count)
-{
-  char *cdst = dst;
-  const char *csrc = src;
-  if(cdst<csrc) {
-      /* move forward */
-      while(count--) *cdst++=*csrc++;
-  } else if(cdst>csrc) {
-      /* move backwards */
-      cdst += count;
-      csrc += count;
-      while(count--) *--cdst=*--csrc;
-  }
-}
+static const uint32_t roval = 0x12345678;
 
-static inline
-void memset(void *dst, int val, size_t count)
-{
-  char *cdst = dst;
-  while(count--) *cdst++=(char)val;
-}
-
-static inline
-void uart_set(uint16_t offset, uint16_t value)
-{
-    volatile uint16_t *addr = (uint16_t*)(offset+0x101f1000);
-    *addr = value;
-}
-
-static inline
-uint16_t uart_get(uint16_t offset)
-{
-    volatile uint16_t *addr = (uint16_t*)(offset+0x101f1000);
-    return *addr;
-}
-
-static inline
-void putchar(char c)
-{
-    uart_set(0, c);
-}
-
-static
-void puts(const char* msg)
-{
-    char c;
-    while( (c=*msg++) )
-    {
-        putchar(c);
-    }
-}
+void xputs(const char*);
+void xnputs(const char*, uint32_t);
+void xputchar(char);
+void halt(void);
 
 static __attribute__((unused))
 void putval(uint32_t v)
@@ -77,25 +28,74 @@ void putval(uint32_t v)
     uint8_t n = sizeof(v)*2;
 
     while(n--) {
-        putchar(hex[v>>28]);
+        xputchar(hex[v>>28]);
         v<<=4;
     }
 }
 
-void Init(void)
+void Init(uint32_t id, uint32_t *info)
 {
-    /* zero out BSS section */
-    memset(&__bss_start, 0, &__bss_end-&__bss_start);
-    /* copy from ROM to RAM.
-     * regions may overlap
-     */
-    memmove(&__data_start, &__data_load, &__data_end-&__data_start);
+    putval(id);
+    xputs("  ");
+    putval((uint32_t)info);
+    xputs("\r\n");
 
-    /* ARM isn't big on introspection, so no idea what peripherials to expect */
+    /* check that .bss and .data are setup correctly */
+    putval(ioportbase); /* expect zero */
+    xputs("\r\n");
+    putval(foobar); /* expect zero */
+    xputs("\r\n");
+    putval(ioportbase2);
+    xputs("\r\n");
+    putval(foobar2);
+    xputs("\r\n");
+    putval(roval);
+    xputs("\r\n");
+
+    if(info && (info[1]&0x54400000)==0x54400000) {
+        uint32_t alen, atag;
+        xputs("Have ATAG board info\r\n");
+
+        while(1) {
+            uint32_t *data;
+            alen = info[0];
+            atag = info[1];
+            if(atag==0)
+                break;
+            data = &info[2];
+            info += alen;
+
+            xputs("ATAG 0x");
+            putval(atag);
+            xputs(" length 0x");
+            putval(alen);
+            xputs("\r\n");
+
+            switch(atag) {
+            case 0x54410002: /* ATAG_MEM */
+                xputs(" Ram size 0x");
+                putval(*data);
+                xputs(" MB\r\n");
+                break;
+            case 0x54410009: /* ATAG_CMDLINE */
+                xputs(" Cmdline ");
+                xnputs((char*)data, 4*(alen-2));
+                xputs("\r\n");
+                break;
+            case 0x54410001: /* ATAG_CORE */
+            case 0x54420005: /* ATAG_INITRD2 */
+            case 0x414f4d50: /* ATAG_BOARD */
+            default:
+                break; /* ignore */
+            }
+        }
+
+
+    } else {
+        xputs("No ATAG board info\r\n");
+    }
 
     /* defaults for PL011 UART work for QEMU */
 
-    puts("hello world!\r\n");
-
-    while(1) {}
+    xputs("hello world!\r\n");
 }
