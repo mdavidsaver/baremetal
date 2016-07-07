46cce79c72aa41175e15c1913dfde35882f0a543
Michael Davidsaver <mdavidsaver@gmail.com>
2016-07-07 17:47:36 -0400

    travis-ci build powerpc

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -x
+
+die() {
+  echo "$1" >&2
+  exit 1
+}
+
+ORIGDIR="$PWD"
+
+PREFIX=/usr/bin
+
+if [ "$ARCH" != "i386"  ];then
+  XDIR="/home/travis/.cache/x-tools/$ARCH"
+  [ -e "$ORIGDIR/$ARCH.config" ] || die "Missing $ARCH.config"
+  if [ ! -e "$XDIR/built" ]; then
+    rm -rf "$XDIR"
+    install -d "$XDIR"
+    cd "$XDIR"
+
+    curl http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.22.0.tar.xz | tar -xJ
+    cd crosstool-ng*
+    ./configure --prefix "$XDIR/usr"
+    make -j2
+    make install
+
+    CTDIR="$PWD"
+
+    mkdir ../ct
+    cd ../ct
+
+    cp "$ORIGDIR/$ARCH.config" .config
+    PATH=$CTDIR:$PATH ct-ng build CT_PREFIX="$XDIR/usr" || (tail -n100 build.log; exit 1)
+    PREFIX="$XDIR/usr"
+    touch "$XDIR/built"
+  fi
+fi
+
+cd "$ORIGDIR"
+echo "toolchain prefix $PREFIX"
+
+make -C "$ARCH" PREFIX=$PREFIX
