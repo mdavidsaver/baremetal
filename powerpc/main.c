08e998a9c0d86bc48c6c58f89dac35b2f0919f4b
Michael Davidsaver <mdavidsaver@gmail.com>
2016-09-08 15:59:41 -0400

    ppc: fixup prep

--- powerpc/main.c
+++ powerpc/main.c
@@ -106,14 +106,6 @@ void nvram_get_string(uint16_t addr, char *buf, size_t len)
 
 void Init(void)
 {
-    /* zero out BSS sections */
-    memset(&__bss_start, 0, &__bss_end-&__bss_start);
-    memset(&__sbss_start, 0, &__sbss_end-&__sbss_start);
-    memset(&__sbss2_start, 0, &__sbss2_end-&__sbss2_start);
-    /* copy from ROM to RAM */
-    memcpy(&__data_start, &__data_load, &__data_end-&__data_start);
-    memcpy(&__sdata_start, &__sdata_load, &__sdata_end-&__sdata_start);
-    memcpy(&__sdata2_start, &__sdata2_load, &__sdata2_end-&__sdata2_start);
     /* setup the 16550 UART */
     outb(0x3fb, 3); /* 8N1, no break, normal access 0b00000011 */
 
